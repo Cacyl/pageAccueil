a61d39e5567643b3627dc10d3e2cd9fe43280a17
Cacyl <cmmouckala@gmail.com>
2015-06-24 16:00:35 +0200

    suppression de fichier en trop

--- c_menu2.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['nom'])&&isset($_POST['prenom'])&&isset($_POST['mdp'])){
-    $nom = htmlentities($_POST['nom']);
-    $prenom = htmlentities($_POST['prenom']);
-    $password = htmlentities($_POST['mdp']);
-    insert($nom,$prenom,$password);
-}
-
-?>
